94f2f6eea0ef142f3f7e553241d5c01f91d5302f
d-smith <doug.smith.mail@gmail.com>
2014-02-19 16:27:52 -0700

    Updated code to reuse prepared statement

--- src/main/scala/dumpreader/Persistor.scala
+++ src/main/scala/dumpreader/Persistor.scala
@@ -1,20 +1,22 @@
 package dumpreader
 
-import java.sql.{DriverManager, Connection}
+import java.sql.{PreparedStatement, DriverManager, Connection}
 import org.slf4j.LoggerFactory
 
 object Persistor {
   var connection: Connection = _
   val insertSQL = "insert into service_call_dump (txn_timestamp, service_name, request, response) values (?,?,XMLType(?),XMLType(?))"
   val logger = LoggerFactory.getLogger(this.getClass)
+  var preparedStmt:PreparedStatement = _
 
   def setDBCredentials(user: String, password: String, url: String) {
     connection = DriverManager.getConnection(url, user, password)
+    preparedStmt = connection.prepareStatement(insertSQL)
   }
 
   def persist(timestamp: Double, serviceName: String, request: String, response: String) {
     try {
-      val preparedStmt = connection.prepareStatement(insertSQL)
+      //val preparedStmt = connection.prepareStatement(insertSQL)
       preparedStmt.setDouble(1, timestamp)
       preparedStmt.setString(2, serviceName)
       /* Can't set string - ORA-01461 results in some cases
@@ -30,7 +32,7 @@ object Persistor {
       preparedStmt.setClob(4, responseClob)
 
       preparedStmt.executeUpdate()
-      preparedStmt.close()
+      //preparedStmt.close()
     } catch {
       case t:Throwable =>
         logger.warn(s"caught exception ${t.getMessage} for $timestamp, $serviceName, $request, $response")
